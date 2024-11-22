845dbb29b31ffb1f0a860f2ddc1dc5d2fd933530
Isa Westling <isawestling.blogg@hotmail.com>
2024-11-22 19:15:13 +0100

    Inkluderat hapi, mongoose och dotenv. Skapat funktion för att starta hapi-server samt inkluderat routes.

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Inkludera Hapi, Mongoose och dotenv
+const Hapi = require("@hapi/hapi");
+const Mongoose = require("mongoose");
+require("dotenv").config();
+
+// Funktion för att starta servern
+const init = async () => {
+
+    // Skapa en ny server och använd port och host från .env eller standardvärden
+    const server = Hapi.server({
+        port: process.env.PORT || 5000,
+        host: process.env.HOST || "localhost"
+    });
+
+    // Anslut till MongoDB
+    Mongoose.connect(process.env.DATABASE).then(() => {
+        console.log("Ansluten till MongoDB");
+    }).catch(error => {
+        console.error("Fel vid anslutning till MongoDB: ", error);
+    });
+
+    // Inkludera och registrera alla routes för resor och skicka med servern
+    require("./routes/travel.route")(server);
+
+    await server.start();
+    console.log("Server körs på %s", server.info.uri);
+};
+
+// Fånga upp eventuella fel
+process.on("unhandledRejection", (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+// Starta servern
+init();
